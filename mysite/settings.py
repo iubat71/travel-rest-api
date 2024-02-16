be14675e4d9a8482e1ab76395d870905f48028d6
salimiubat <salimliveme@gmail.com>
2024-02-16 16:23:33 +0600

    17_feb_core_headers

--- mysite/settings.py
+++ mysite/settings.py
@@ -53,6 +53,7 @@ SIMPLE_JWT = {
     }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -61,7 +62,9 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  
+]
 ROOT_URLCONF = "mysite.urls"
 
 TEMPLATES = [
